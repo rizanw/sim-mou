c4067434d986b546342e2330807d29c60a291e37
rizanw <rzkandre@gmail.com>
2022-03-20 12:14:52 +0700

    add program models

--- app/Models/Program.php
+++ app/Models/Program.php
@@ -5,16 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Document extends Model
+class Program extends Model
 {
     use HasFactory;
 
     /**
-     * Get the programs for the Document.
+     * Get the documents for the Program.
      * many-to-many relationship
      */
-    public function programs()
+    public function documents()
     {
-        return $this->belongsToMany(Program::class, 'documents_programs', 'document_id', 'program_id');
+        return $this->belongsToMany(Document::class, 'documents_programs', 'program_id', 'document_id');
     }
 }
